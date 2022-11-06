2f56b103ed7fff4dc6c5e86d10c0df7ef59df4c5
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2022-11-06 19:56:33 +0000

    auth jwt
    auth jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,16 +7,22 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     // generate the password hash
+    const hash = await argon.hash(dto.password);
 
     try {
-      const hash = await argon.hash(dto.password);
       // save thr new user in the database
       const user = await this.prisma.user.create({
         data: {
@@ -25,9 +31,7 @@ export class AuthService {
         },
       });
 
-      delete user.hash;
-      // return the saved user
-      return user;
+      return this.signToken(user.id, user.email);
     } catch (error) {
       if (
         error instanceof
@@ -62,12 +66,33 @@ export class AuthService {
       dto.password,
     );
     // if password incorrect throw an exception
-        if (!pwMatches)
-        throw new ForbiddenException(
-            'Credentials incorrects',
-          );
-    // send back the user
-    delete user.hash;
-    return user;
+    if (!pwMatches)
+      throw new ForbiddenException(
+        'Credentials incorrects',
+      );
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{access_token: string}> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(
+      payload, 
+      {
+      expiresIn: '15m',
+      secret: secret,
+    },
+    );
+
+    return {
+      access_token: token,
+    };
   }
 }
