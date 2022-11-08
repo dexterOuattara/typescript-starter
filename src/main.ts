b03b83f2d8982f3f656f7b4c8e4a467f82dcfabf
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2022-11-08 00:41:41 +0000

    Swagger update
    Swagger update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle('API NESTJS')
+  .setDescription('API NESTJS API description')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe(
    { whitelist: true}
   ))
