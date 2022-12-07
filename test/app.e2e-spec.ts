8cc3d7af62046ecae7e48ab9a2fabc8258c9eafb
dexterOuattara <31624715+dexterOuattara@users.noreply.github.com>
2022-12-07 10:48:34 +0000

    Update app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import { Test } from "@nestjs/testing"
 import { AppModule } from "./../src/app.module";
 
 describe('App e2e', () => {
   it.todo('should pass')
 });
-=======
->>>>>>> f9e48dd3a7f62865364a7cb81114f0f8cdaba3f5
